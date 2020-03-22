cb09ebb99afafe8d714459d3c3ea671b60d6391d
marieke91 <mariekepots@gmail.com>
2020-03-22 16:43:42 +0100

    Create cloudfunction_datatransfer.py

---
+++ cloudfunction_datatransfer.py
@@ -0,0 +1,14 @@
+import time
+from google.protobuf.timestamp_pb2 import Timestamp
+from google.cloud import bigquery_datatransfer_v1
+
+def runQuery (parent, requested_run_time):
+    client = bigquery_datatransfer_v1.DataTransferServiceClient()
+    projectid = '[enter your projectId here]' # Enter your projectID here
+    transferid = '[enter your transferId here]'  # Enter your transferId here
+    parent = client.project_transfer_config_path(projectid, transferid)
+    start_time = bigquery_datatransfer_v1.types.Timestamp(seconds=int(time.time() + 10))
+    response = client.start_manual_transfer_runs(parent, requested_run_time=start_time)
+    print(response)
+    
+# do not forget to put google-cloud-bigquery-datatransfer>=0.1 in the requirements.txt
